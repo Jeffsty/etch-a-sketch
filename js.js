d76184b604656ce396966cbad5fab80d2b4565c4
Jeffsty <kirsty.sees@gmail.com>
2022-10-19 21:19:52 +0100

    Add mouseover colouring

--- js.js
+++ js.js
@@ -1,18 +1,29 @@
-for (let i = 0; i < 16 * 16; i++) {
-    let container = document.querySelector("#container");
+/* for (let i = 0; i < 16 * 16; i++) {
+    let container = document.querySelector("#grid");
     let content = document.createElement("div");
     container.appendChild(content);
     content.classList.add("content");
     content.textContent = ".";
-}
+} */
 
-document.getElementById("container").addEventListener("mouseover", mouseOver);
-document.getElementById("container").addEventListener("mouseout", mouseOut);
+const grid = document.querySelector(".grid");
+// try it again but without arrow function, to prove understanding
+createGrid =() => {
+    for (let i = 0; i < 16 * 16; i++) {
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        div.addEventListener("mouseover", function(event){
+            event.target.style.backgroundColor = "black";
+        })
+        grid.appendChild(div);
+    }
 
-function mouseOver() {
-    document.getElementById("container").style.backgroundColor = "black";
-  }
+}; 
 
-function mouseOut() {
-    document.getElementById("container").style.backgroundColor = "white";
-}
+
+
+  /*Try this way tomorrow ^^^
+  */
+
+
+  createGrid();
