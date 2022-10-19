721e376331de6dff470c97ff619bd11ecb4ef241
Jeffsty <kirsty.sees@gmail.com>
2022-10-19 16:36:04 +0100

    Add mouseOver and mouseOut

--- js.js
+++ js.js
@@ -5,3 +5,14 @@ for (let i = 0; i < 16 * 16; i++) {
     content.classList.add("content");
     content.textContent = ".";
 }
+
+document.getElementById("container").addEventListener("mouseover", mouseOver);
+document.getElementById("container").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("container").style.backgroundColor = "black";
+  }
+
+function mouseOut() {
+    document.getElementById("container").style.backgroundColor = "white";
+}
